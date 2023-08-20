77bb3a0f54bdf0c413f5303008267678cfcb7a4e
luis <lgarciaag89@gmail.com>
2023-08-20 14:22:09 -0700

    update add hydrogen

--- src/main/java/taller/App.java
+++ src/main/java/taller/App.java
@@ -1,23 +1,19 @@
 package taller;
 
-import org.openscience.cdk.atomtype.CDKAtomTypeMatcher;
 import org.openscience.cdk.depict.DepictionGenerator;
 import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtomContainerSet;
-import org.openscience.cdk.interfaces.IAtomType;
+import org.openscience.cdk.layout.StructureDiagramGenerator;
 import org.openscience.cdk.tools.CDKHydrogenAdder;
-import org.openscience.cdk.tools.manipulator.AtomTypeManipulator;
-import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import javax.swing.*;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
 
-public class App 
-{
+public class App {
     public static void drawMolecule(IAtomContainer molecule) throws Exception {
         DepictionGenerator dptgen = new DepictionGenerator();
         dptgen.withSize(200, 250)
@@ -37,41 +33,43 @@ public class App
         dialog.setVisible(true);
     }
 
-    public static void addH(IAtomContainer mol) throws CDKException {
+    public static IAtomContainer addH(IAtomContainer molecule) throws CDKException {
 
-        CDKAtomTypeMatcher matcher = CDKAtomTypeMatcher.getInstance(SilentChemObjectBuilder.getInstance());
-        for (IAtom atom : mol.atoms()) {
-            IAtomType type = matcher.findMatchingAtomType(mol, atom);
-            AtomTypeManipulator.configure(atom, type);
-        }
-        CDKHydrogenAdder adder = CDKHydrogenAdder.getInstance(mol.getBuilder());
-        adder.addImplicitHydrogens(mol);
+        AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(molecule);
+        CDKHydrogenAdder adder = CDKHydrogenAdder.getInstance((molecule).getBuilder());
+        adder.addImplicitHydrogens(molecule);
+        AtomContainerManipulator.convertImplicitToExplicitHydrogens(molecule);
+        StructureDiagramGenerator sdg = new StructureDiagramGenerator(molecule);
+        sdg.generateCoordinates();
+        return sdg.getMolecule();
     }
-    public static void printInfo(IAtomContainerSet mols) throws Exception {
-        System.out.println("There are "+mols.getAtomContainerCount()+" molecules");
 
-        for(int i = 0 ; i < mols.getAtomContainerCount() ; i++){
+    public static void printInfo(IAtomContainerSet mols) throws Exception {
+        System.out.println("There are " + mols.getAtomContainerCount() + " molecules");
+
+        for (int i = 0; i < mols.getAtomContainerCount(); i++) {
             IAtomContainer mol = mols.getAtomContainer(i);
-            System.out.println("Molecule "+i+" has "+mol.getAtomCount()+" atoms");
+            System.out.println("Molecule " + i + " has " + mol.getAtomCount() + " atoms");
         }
 
         IAtomContainer mol = mols.getAtomContainer(0);
 
-        addH(mol);
+        mol = addH(mol);
 
 
-        for(int i = 0 ; i < mol.getAtomCount() ; i++){
-            String isInRing = mol.getAtom(i).isInRing() ? "Is in Ring":"Is not in Ring";
-            String isAromatic = mol.getAtom(i).isAromatic() ? "Is an aromatic atom":"Is not an aromatic atom";
-            System.out.println("Atom "+i+" has symbol "+mol.getAtom(i).getSymbol()+","
-                    + " valence "+ mol.getAtom(i).getValency()+","
-                    + isInRing +", "
+        for (int i = 0; i < mol.getAtomCount(); i++) {
+            String isInRing = mol.getAtom(i).isInRing() ? "Is in Ring" : "Is not in Ring";
+            String isAromatic = mol.getAtom(i).isAromatic() ? "Is an aromatic atom" : "Is not an aromatic atom";
+            System.out.println("Atom " + i + " has symbol " + mol.getAtom(i).getSymbol() + ","
+                    + " valence " + mol.getAtom(i).getValency() + ","
+                    + isInRing + ", "
                     + isAromatic);
         }
         drawMolecule(mol);
 
     }
-    public static void main( String[] args ) throws Exception {
+
+    public static void main(String[] args) throws Exception {
         String csvFile = "DILI-Liew_2_B_TS_47.csv";
         String sdfFile = "2_B_TS_47_act.sdf";
 
@@ -79,9 +77,9 @@ public class App
 
         printInfo(mols);
 
-//        IAtomContainerSet molsSdf = ReadMol.readSdf(sdfFile);
-//
-//        printInfo(molsSdf);
+        IAtomContainerSet molsSdf = ReadMol.readSdf(sdfFile);
+
+        printInfo(molsSdf);
 
     }
 }
